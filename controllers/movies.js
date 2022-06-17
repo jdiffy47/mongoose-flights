04801b53bdfa562313cd837b6eef183c4615a80c
David Stinson <49245298+DavidStinson@users.noreply.github.com>
2022-06-17 15:51:01 -0500

    add data validation

--- controllers/movies.js
+++ controllers/movies.js
@@ -7,13 +7,17 @@ function newMovie(req, res) {
 }
 
 function create(req, res) {
+  console.log("REQ.BODY:", req.body)
   req.body.nowShowing = !!req.body.nowShowing
   if (req.body.cast) {
     req.body.cast = req.body.cast.split(", ")
   }
+  for (let key in req.body) {
+	  if (req.body[key] === '') delete req.body[key]
+	}
   Movie.create(req.body)
   .then(movie => {
-    console.log(movie)
+    console.log("CREATED MOVIE:", movie)
     // SEND A GET REQUEST TO THIS URL!
     res.redirect(`/movies`)
   })
--- models/movie.js
+++ models/movie.js
@@ -3,11 +3,23 @@ import mongoose from "mongoose"
 const Schema = mongoose.Schema
 
 const movieSchema = new Schema({
-  title: String,
-  releaseYear: Number,
-  mpaaRating: String,
+  title: {
+    type: String,
+    required: true
+  },
+  releaseYear: { 
+    type: Date, 
+    default: function () {
+      return new Date().getFullYear()
+    },
+    min: 1927
+  },
+  mpaaRating: {
+    type: String,
+    enum: ["G", "PG", "PG-13", "R"]
+  },
   cast: [String],
-  nowShowing: Boolean,
+  nowShowing: { type: Boolean, default: false },
 }, {
   timestamps: true,
 })
