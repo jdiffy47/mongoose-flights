c87147c845fb4db7a9917eb424b486d840554cd4
JakeD <jakediefenderfer@gmail.com>
2022-06-20 10:09:03 -0400

    structure new.ejs file

--- controllers/flights.js
+++ controllers/flights.js
@@ -1,31 +1,31 @@
-import { Flight } from "../models/flight"
+import { Flight } from "../models/flight.js"
 
-function newFlight(req, res) {
-  res.render("flights/new", {
-    title: "Add Flight"
-  })
-}
+// function newFlight(req, res) {
+//   res.render("flights/new", {
+//     title: "Add Flight"
+//   })
+// }
 
-function create(req, res) {
-  console.log("REQ.BODY:", req.body)
-  req.body.nowShowing = !!req.body.nowShowing
-  if (req.body.cast) {
-    req.body.cast = req.body.cast.split(", ")
-  }
-  for (let key in req.body) {
-	  if (req.body[key] === '') delete req.body[key]
-	}
-  Flight.create(req.body)
-  .then(flight => {
-    console.log("CREATED FLIGHT:", flight)
-    // SEND A GET REQUEST TO THIS URL!
-    res.redirect(`/flights`)
-  })
-  .catch(err => {
-    console.log(err)
-    res.redirect('/movies')
-  })
-}
+// function create(req, res) {
+//   console.log("REQ.BODY:", req.body)
+//   req.body.nowShowing = !!req.body.nowShowing
+//   if (req.body.cast) {
+//     req.body.cast = req.body.cast.split(", ")
+//   }
+//   for (let key in req.body) {
+// 	  if (req.body[key] === '') delete req.body[key]
+// 	}
+//   Flight.create(req.body)
+//   .then(flight => {
+//     console.log("CREATED FLIGHT:", flight)
+//     // SEND A GET REQUEST TO THIS URL!
+//     res.redirect(`/flights`)
+//   })
+//   .catch(err => {
+//     console.log(err)
+//     res.redirect('/movies')
+//   })
+// }
 
 function index(req, res) {
   Flight.find({})
@@ -38,7 +38,7 @@ function index(req, res) {
 }
 
 export {
-  newFlight as new,
-  create,
-  index,
+  // newFlight as new,
+  // create,
+  index
 }
